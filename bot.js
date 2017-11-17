a31a42a81553b0acfce9f7a0010d8ba7db899a08
Vasyl Zadorozhnyy <amdpaxton@gmail.com>
2017-11-17 10:43:51 +0200

    invite generator created, with old version

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const config = require("./config.json");
 const Discord = require("discord.js");
+const config = require("./config.json");
 const bot = new Discord.Client({disableEveryone: true});
 const prefix = config.prefix;
 
